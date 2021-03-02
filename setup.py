467b6845847439f45ae2dccfad745ba537cd2356
VadyChel <zyzel19@gmail.com>
2021-03-02 11:27:26 +0200

    Upload to a pypi

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='mbc',
+    version='0.1',
+    description='Wrapper for the moonbots api',
+    packages=['mbc'],
+    author_email='zyzel19@gmail.com',
+    zip_safe=False
+)
