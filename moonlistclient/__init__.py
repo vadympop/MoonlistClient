50c9833c2abfda123061443e22eb4c856dd3b111
VadyChel <zyzel19@gmail.com>
2021-03-15 15:29:16 +0200

    Oops... Fix imports x3

--- moonlistclient/__init__.py
+++ moonlistclient/__init__.py
@@ -2,6 +2,5 @@ from .client import MoonlistClient
 from .exceptions import *
 from .http import ApiClient
 from .webhook_manager import *
-from . import models
 from .models.api import *
 from .models.webhooks import *
