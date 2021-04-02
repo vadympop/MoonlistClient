5fbc12b560c9b8c8b26f70934eea77039e7176fc
VadyChel <zyzel19@gmail.com>
2021-04-02 10:12:54 +0300

    Oops

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='moonlistclient',
-    version='0.6.6',
+    version='0.6.7',
     description='Wrapper for the moonlist api',
     long_description=long_description,
     long_description_content_type="text/markdown",
